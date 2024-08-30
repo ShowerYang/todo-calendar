772a7e4c362f361b24bea705e0727fa25212985c
ShowerYang <mark00120@hotmail.com>
2024-08-30 11:59:33 +0800

    修改router設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 /* pages */
 import LoginView from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
@@ -6,7 +6,8 @@ import HomeView from '../views/HomeView.vue'
 import { useCurrentUser, getCurrentUser } from 'vuefire'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
